709bc8774a3682fc8395841f642579ccbddf00ea
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2025-06-11 08:29:01 -0600

    agregue los estilos y cree la otra pantalla para registro y su controlador

---
+++ app/src/main/java/neri/saul/thecheezery_nerisaul/MainActivity.kt
@@ -0,0 +1,22 @@
+package neri.saul.thecheezery_nerisaul
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val getStartedBtn: Button = findViewById(R.id.get_started_btn)
+
+        getStartedBtn.setOnClickListener {
+            val intent = Intent(this, UserRegistryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
