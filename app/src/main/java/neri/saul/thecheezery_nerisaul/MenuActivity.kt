1f47fad78df64c34576e937876598e3d7b554e58
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2025-06-11 15:34:25 -0600

    cambios finales, ya funciona la aplicacion

---
+++ app/src/main/java/neri/saul/thecheezery_nerisaul/MenuActivity.kt
@@ -0,0 +1,13 @@
+package neri.saul.thecheezery_nerisaul
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity :  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+    }
+}
--- app/src/main/java/neri/saul/thecheezery_nerisaul/UserRegistryActivity.kt
+++ app/src/main/java/neri/saul/thecheezery_nerisaul/UserRegistryActivity.kt
@@ -1,6 +1,8 @@
 package neri.saul.thecheezery_nerisaul
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,8 +10,14 @@ class UserRegistryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_user_registry)
+        setContentView(R.layout.activity_register)
 
+        val signInBtn: Button = findViewById(R.id.sign_in_btn)
+
+        signInBtn.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
